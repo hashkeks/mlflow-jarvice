f7807c6c396a3fb7312317f829b7ac91f7c521d0
Cedric Casper <cedriccasper@protonmail.ch>
2022-07-05 18:38:25 +0200

    Added Miniconda install script

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Install Miniconda
+# 	https://conda.io/projects/conda/en/latest/user-guide/install/macos.html#install-macos-    silent
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O minicon    da.sh
 bash miniconda.sh -b -p /home/nimbix/miniconda
